02d0c5cfe7f4814dee9dbf1dbb6724e3baee5053
Yixian Du <duyixian1234@qq.com>
2021-12-21 20:03:37 +0800

    Data Structures

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,62 @@
-fn apply(value: i32, f: fn(i32) -> i32) -> i32 {
-    f(value)
+#[derive(Debug)]
+enum Gender {
+    Unspecified = 0,
+    Female = 1,
+    Male = 2,
 }
 
-fn square(value: i32) -> i32 {
-    value * value
+#[derive(Debug, Copy, Clone)]
+struct UserId(u64);
+
+#[derive(Debug, Clone, Copy)]
+struct TopicId(u64);
+
+#[derive(Debug)]
+struct User {
+    id: UserId,
+    name: String,
+    gender: Gender,
 }
 
-fn cube(value: i32) -> i32 {
-    value * value * value
+#[derive(Debug)]
+struct Topic {
+    id: TopicId,
+    name: String,
+    owner: UserId,
 }
 
-fn pi() -> f64 {
-    3.1415926
-}
-
-fn not_pi() {
-    3.1415926;
+#[derive(Debug)]
+enum Event {
+    Join((UserId, TopicId)),
+    Leave((UserId, TopicId)),
+    Message((UserId, TopicId, String)),
 }
 
 fn main() {
-    println!("apply square: {}", apply(2, square));
-    println!("apply cube: {}", apply(2, cube));
-
-    let is_pi = pi();
-    let is_unit1 = not_pi();
-    let is_unit2 = {
-        pi();
+    let alice = User {
+        id: UserId(1),
+        name: "Alice".into(),
+        gender: Gender::Female,
+    };
+    let bob = User {
+        id: UserId(2),
+        name: "Bob".into(),
+        gender: Gender::Male,
     };
 
-    println!("is_pi: {:?}, is_unit1: {:?}, is_unit2: {:?}", is_pi,is_unit1,is_unit2);
+    let topic = Topic {
+        id: TopicId(1),
+        name: "rust".into(),
+        owner: UserId(1),
+    };
+
+    let event1 = Event::Join((alice.id, topic.id));
+    let event2 = Event::Join((bob.id, topic.id));
+    let event3 = Event::Message((alice.id, topic.id, "Hello, world!".into()));
+    let event4 = Event::Leave((alice.id, topic.id));
+
+    println!(
+        "event1: {:?}, event2: {:?}, event3: {:?}, event4: {:?}",
+        event1, event2, event3, event4
+    );
 }
