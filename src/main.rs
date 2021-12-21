d42dc3e5ba1224768739846cf60fc694ec758e99
Yixian Du <duyixian1234@qq.com>
2021-12-21 19:52:39 +0800

    basic types

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
-use std::fs;
+fn apply(value: i32, f: fn(i32) -> i32) -> i32 {
+    f(value)
+}
+
+fn square(value: i32) -> i32 {
+    value * value
+}
+
+fn cube(value: i32) -> i32 {
+    value * value * value
+}
+
+fn pi() -> f64 {
+    3.1415926
+}
+
+fn not_pi() {
+    3.1415926;
+}
+
 fn main() {
-    let url = "https://www.rust-lang.org/";
-    let output = "rust.md";
+    println!("apply square: {}", apply(2, square));
+    println!("apply cube: {}", apply(2, cube));
 
-    println!("Fetching url: {}", url);
-    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let is_pi = pi();
+    let is_unit1 = not_pi();
+    let is_unit2 = {
+        pi();
+    };
 
-    println!("Converting html to markdown...");
-    let md = html2md::parse_html(&body);
-
-    fs::write(output, md.as_bytes()).unwrap();
-    println!("Converted markdown has been saved in {}", output);
-
+    println!("is_pi: {:?}, is_unit1: {:?}, is_unit2: {:?}", is_pi,is_unit1,is_unit2);
 }
